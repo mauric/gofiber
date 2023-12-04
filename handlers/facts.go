ece5b37890f834bdb4b4d1bb381b9745fdcddc24
Mauric <mauric.caceres@gmail.com>
2023-12-04 20:24:10 -0300

    add facts CRUD

--- handlers/facts.go
+++ handlers/facts.go
@@ -1,12 +1,29 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mauric/gofiber/database"
+	"github.com/mauric/gofiber/models"
+)
 
-func Home(c *fiber.Ctx) error {
-	return c.SendString("hello, mauric here")
+func ListFacts(c *fiber.Ctx) error {
+
+	facts := []models.Fact{}
+
+	database.DB.Db.Find(&facts)
+
+	return c.Status(200).JSON(facts)
 }
 
-// Handler old version
-func hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, Argentina 👋!")
+func CreateFact(c *fiber.Ctx) error {
+	fact := new(models.Fact)
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	database.DB.Db.Create(&fact)
+	return c.Status(200).JSON(fact)
+
 }
